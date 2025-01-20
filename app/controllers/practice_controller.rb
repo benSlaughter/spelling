84bf4fd7a64f6ce038b0f21bd316c82c237bfe45
Ben Slaughter <benslaughter@github.com>
2025-01-20 19:57:24 +0000

    Fix practice bug

--- app/controllers/practice_controller.rb
+++ app/controllers/practice_controller.rb
@@ -2,32 +2,44 @@ class PracticeController < ApplicationController
   allow_unauthenticated_access
   include Rails.application.routes.url_helpers
 
+  helper_method :message
+  helper_method :is_correct?
+
   def show
     @pagehead = "Spellings practice"
     @week = Week.find_by(id: params[:week_id])
     @word = Word.find_by(id: params[:id])
-    @result = params[:result]
+    @correct = params[:correct]
 
     redirect_to "/" unless @week.words.include?(@word)
   end
 
   def update
-    @pagehead = "Spellings practice"
+    @pagehead = "Spellings again"
     @week = Week.find_by(id: params[:week_id])
     @word = Word.find_by(id: params[:id])
 
     if word_params[:title].downcase == @word.title.downcase
       if @word.next_word.nil?
-        render :result
+        render :result, status: 202
       else
-        redirect_to week_practice_path(week_id: @week.id, id: @word.next_word.id, result: "Correct!")
+        redirect_to week_practice_path(week_id: @week.id, id: @word.next_word.id, correct: true)
       end
     else
-      @word.errors.add(:title, "incorrect")
-      @result = "Oops, try again!"
-      render :show
+      redirect_to week_practice_path(week_id: @week.id, id: @word.id, correct: false)
     end
-      
+  end
+
+  def is_correct?
+    @correct == "true"
+  end
+
+  def message
+    if @correct == "true"
+      "Correct, well done!"
+    else
+      "Oops, try again!"
+    end
   end
 
   def word_params
